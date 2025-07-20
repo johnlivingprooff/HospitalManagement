ca445045e2e29a7d83832f01ccfef93816513e89
johnlivingprooff <johnlivingprooff@gmail.com>
2025-07-20 18:54:20 +0200

    Updating CORS

--- hms-fastapi/backend/app/main.py
+++ hms-fastapi/backend/app/main.py
@@ -45,7 +45,8 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "https://hospitalmanagementmihr.netlify.app"],
+    # allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
